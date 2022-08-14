6316a31877ac8b14ad4e849c33d12781f0916edf
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-14 17:19:54 +0600

    Multiple string concatenate

---
+++ multiple string concatenate.cpp
@@ -0,0 +1,14 @@
+//Multiple string concatenate
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="md";
+    string a= " " ;
+    string b="nasim";
+    string c= " ";
+    string d="hossen";
+    s+=(a+b+c+d);
+    cout<<s<<endl;
+}
