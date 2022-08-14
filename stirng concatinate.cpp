cc05e242768ef7960cceb614908bc4db9aa3e939
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-14 16:58:24 +0600

    concatenate the string after initialize

---
+++ stirng concatinate.cpp
@@ -0,0 +1,17 @@
+//concatenate the string after initialize
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="md nasim";
+    s+=' ';
+    s+='h';
+    s+='o';
+    s+='s';
+    s+='s';
+    s+='e';
+    s+='n';
+    s+= " is a great man";
+    cout<<s<<endl;
+}
