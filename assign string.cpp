fd5262588492117bbe3fe07382b214147f971e58
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-14 17:28:34 +0600

    assigning the string

---
+++ assign string.cpp
@@ -0,0 +1,13 @@
+//assigning the string
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="md nasim hossen";
+    cout<<s<<endl;//before assigning
+    string s1="Bangladesh";
+    s=s1;
+    cout<<s<<endl;//after assigning
+}
+
