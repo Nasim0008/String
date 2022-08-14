c5ce03fef3229135fa5a702f23fc80d80a380533
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-14 17:51:48 +0600

    Compare two string

---
+++ comparism.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="Nasim",p="NASIM";
+    if(s==p)
+    {
+        cout<< "equal"<<endl;
+    }
+    else
+    {
+        cout<< "not equal"<<endl;
+    }
+}
