cca3a68032558af5a3aa56d14bc5c6ce438ceed5
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-14 17:45:27 +0600

    print the string by using for each loop

---
+++ for each loop.cpp
@@ -0,0 +1,14 @@
+//print the string by using for each loop
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="md nasim hossen";
+    for(auto u:s)
+    {
+        cout<<u;
+    }
+    cout<<endl;
+}
+
