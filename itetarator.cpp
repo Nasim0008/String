8f583c89f22a2194c7f5727e6e9de9e275bf9324
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-14 17:41:50 +0600

    print the string by using iterator

---
+++ itetarator.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="md nasim hossen";
+    string :: iterator it;
+    for(it=s.begin();it!=s.end();it++)
+    {
+        cout<<*it;
+    }
+    cout<<endl;
+}
