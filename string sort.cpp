d1b4504c8e9432267b5e183d5b380ae5aa8e69ca
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-16 20:30:57 +0600

    Sort the string by using sort function

---
+++ string sort.cpp
@@ -0,0 +1,10 @@
+//Sort the string by using sort function
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="ganogawGFERREONghawrjnSGFOTRJHfgaharlgbnga";
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+}
