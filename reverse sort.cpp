bd99e4e7bf4f2d636d1d73a17666159a25a46c70
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-16 20:33:10 +0600

    reverse sort the string by using sort function

---
+++ reverse sort.cpp
@@ -0,0 +1,11 @@
+//reverse sort the string by using sort function
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="ganogawGFERREONghawrjnSGFOTRJHfgaharlgbnga";
+    sort(s.rbegin(),s.rend());
+    cout<<s<<endl;
+}
+
