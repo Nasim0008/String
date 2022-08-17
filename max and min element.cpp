cd2c572dbb2ea5f8d63626f225b3c684949e9cb3
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-17 14:50:40 +0600

    Find the max and min element in string

---
+++ max and min element.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="AGRGghlgroz";
+    cout<<*max_element(s.begin(),s.end())<<endl;
+    cout<<*min_element(s.begin(),s.end())<<endl;
+}
