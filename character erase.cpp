06812fafe8c2d9f92c5b1f47bc004ab9f6296688
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-17 14:59:21 +0600

    erase the first charater

---
+++ character erase.cpp
@@ -0,0 +1,11 @@
+// erase the first and charater
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s= "aghrog";
+
+   s.erase(s.begin());//erase the first element
+    cout<<s;
+}
