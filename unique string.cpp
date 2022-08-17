9622c3b9b3e36645579d179bf3a2ce37ee3c7ed2
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-17 14:45:15 +0600

    Find the unique character of a string

---
+++ unique string.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="ORHGHGHGHEWRGTJgalafoihgtlagh";
+    sort(s.begin(),s.end());
+    int sz=unique(s.begin(),s.end())-s.begin();
+    for(int i=0;i<sz;i++)
+    {
+        cout<<s[i];
+    }
+}
