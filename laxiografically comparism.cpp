bae51b942ef26ba615ef1703c99797d29ff47c39
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-17 17:33:23 +0600

    check lexicographically smallest string

---
+++ laxiografically comparism.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a="Nasim",b="Ayesha";
+    if(a<b)
+    {
+        cout<<a<< " is lexicographically smallest"<<endl;
+    }
+    else cout<<b<< " is lexicographically smallest"<<endl;
+}
