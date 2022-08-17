650e8d1798b72b774bb3a678eea992392b17ec5b
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-17 18:06:31 +0600

    lexicographically sorting of multiple string

---
+++ lexicografically sorting.cpp
@@ -0,0 +1,20 @@
+// lexicographically sorting of multiple string
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());//sorting
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i]<< endl;
+    }
+}
+
