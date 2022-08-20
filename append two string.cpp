5dad434b797e97dbc1e4875da7dc3b98b15d30a5
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-20 20:05:07 +0600

    add multiple string

---
+++ append two string.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string st = "md nasim ";
+    string st2 = "hossen";
+    st.append(st2);
+    cout<<st<<endl;
+    st.append( " is a great person");
+    cout<<st;
+}
