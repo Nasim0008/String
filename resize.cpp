d98d71c1cefd0ab1785cd598334cd1d19885b941
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-20 13:00:31 +0600

    resize the string size

---
+++ resize.cpp
@@ -0,0 +1,12 @@
+//find the string size
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="mdnasimhossen";
+    cout<<s<<"  "<<s.size()<<endl;//string size()
+    s.resize(7);
+    cout<<s<< " "<<s.size();
+}
+
