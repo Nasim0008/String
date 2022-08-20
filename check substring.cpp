785e6db24f747b606e2982b177bcbf6474b86c9e
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-21 01:24:47 +0600

    check substring or not

---
+++ check substring.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string st = "Nasim hossen";
+    if(st.find("sim")!= -1) cout<< "Yes,substring";
+    else cout<< "No,Not substring"<<endl;
+}
