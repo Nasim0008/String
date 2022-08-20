364de9961383715ae498d444b9a664452edc48e1
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-20 21:06:40 +0600

    using find function

---
+++ find all.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string st="mdnasimhossen";
+    cout<<st.find_first_of('n')<<endl;
+    cout<<st.find_last_of('s')<<endl;
+    cout<<st.find_first_not_of('m')<<endl;
+    cout<<st.find_last_not_of('n')<<endl;
+}
