fda62aa3afc8b0dc6fc0fc9c537aaa93ef15542d
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-10-20 12:39:10 +0600

    string reverse from begin to end

---
+++ reverse.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main()
+{
+    optimize();
+    string str = "Journal Dev reverse example";
+    reverse(str.begin(), str.end());
+	cout<<str;
+
+}
+
+
