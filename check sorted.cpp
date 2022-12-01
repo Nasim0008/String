fa8b28a16cc4147bbebf6f43bf7f225f75e82600
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-12-01 12:03:58 +0600

    check the string sorted or not

---
+++ check sorted.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string s= "abceag";
+   if(is_sorted(s.begin(),s.end()))
+   {
+       cout<< "Sorted"<<endl;
+   }
+   else
+   {
+       cout<< "Unsorted"<<endl;
+   }
+
+}
+
