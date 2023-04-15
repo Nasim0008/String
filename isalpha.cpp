650a40f7755005c71faa76a277579ed95b969133
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-04-16 00:08:59 +0600

    check the character is alpha or not

---
+++ isalpha.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    if(isalpha(ch))
+    {
+        cout<< "YES"<<endl;
+    }
+    else
+    {
+        cout<< "NO"<<endl;
+    }
+}
